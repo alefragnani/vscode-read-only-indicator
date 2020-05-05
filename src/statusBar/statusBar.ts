0c7432ddd0cb66a137a73c6125039944c9ffe63d
Chris Antos <sparrowhawk996@gmail.com>
2020-05-05 13:55:08 -0700

    Use color contributions.

--- src/statusBar/statusBar.ts
+++ src/statusBar/statusBar.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { StatusBarAlignment, StatusBarItem, window, workspace } from "vscode";
+import { StatusBarAlignment, StatusBarItem, ThemeColor, window, workspace } from "vscode";
 import { FileAccess, UIMode } from "./../constants";
 import { Operations } from "./../operations";
 export class StatusBar {
@@ -39,8 +39,6 @@ export class StatusBar {
         const uimodeString: string = (workspace.getConfiguration("fileAccess").get("uiMode", "complete"));
         const uimode: UIMode = uimodeString === "complete" ? UIMode.Complete : UIMode.Simple;
         const readOnly = fileAccess ? fileAccess === FileAccess.ReadOnly : Operations.isReadOnly(activeDocument);
-        const colorConfigString: string = (workspace.getConfiguration("fileAccess").get(readOnly ? "readonlyColor" : "writableColor", "none"));
-        const colorString: string = colorConfigString === "none" || colorConfigString === "" ? "" : colorConfigString;
 
         // Update the status bar
         if (uimode === UIMode.Complete) {
@@ -48,9 +46,7 @@ export class StatusBar {
         } else {
             this.statusBarItem.text = !readOnly ? "RW" : "RO";
         }
-        if (colorString != "") {
-            this.statusBarItem.color = colorString;
-        }
+        this.statusBarItem.color = new ThemeColor(readOnly ? "fileAccess.readonlyForeground" : "fileAccess.writableForeground");
 
         this.statusBarItem.tooltip = !readOnly ? "The file is writeable" : "The file is read only";
 
